805b4231cb69fbe271457539bd092c9361e3b116
kg6725 <kgarubba@benco.com>
2020-06-05 22:37:05 -0400

    Fixing the babel to transpile for older browsers like IE11 properly.

--- src/js/script.js
+++ src/js/script.js
@@ -1,141 +1,123 @@
-import '../sass/style.scss';
+import "../sass/style.scss";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Rellax from 'rellax';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Rellax from "rellax";
 
-import './functions/touch-keyboard-navigation';
-import './functions/activate-search';
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
 
-AOS.init();
-
-
+import "./functions/touch-keyboard-navigation";
+import "./functions/activate-search";
 
 // Scroll to specific values
 // scrollTo is the same
-
-function scrollIt(destination, duration = 200, easing = 'linear', callback) {
+function scrollIt(destination, duration = 200, easing = "linear", callback) {
     // object with some some timing functions
     // function body here
     const start = window.pageYOffset;
-    const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-    
-}
-var scrollE = document.querySelectorAll('.jump-scroll')[0];
-    if (scrollE !== undefined || scrollE !== null){
-    scrollE.addEventListener('click', event => {
-        if (!scrollE.classList.contains('clicked')){
-            scrollE.classList.add('clicked');
-        }
-    });
+    const startTime =
+        "now" in window.performance ? performance.now() : new Date().getTime();
 }
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
+    AOS.init();
 
-    if( document.querySelectorAll('.rellax').length > 0 ) {
-
-        var rellax = new Rellax('.rellax', {
+    if (document.querySelectorAll(".rellax").length > 0) {
+        var rellax = new Rellax(".rellax", {
             center: true,
         });
     }
 
-    // Create dropdown for homepage feature section
-    var navThang = document.querySelectorAll('.feature-page-subnav')[0],
-        subNavThang = document.querySelectorAll('.feature-page-subnav .subnav')[0];
-
-    if(navThang){
-        navThang.addEventListener('mouseenter', function(event){
-            subNavThang.classList.toggle('active') ;
-        });
-
-        navThang.addEventListener('mouseleave', function(event){
-            subNavThang.classList.toggle('active') ;
+    var scrollE = document.querySelectorAll(".jump-scroll")[0];
+    if (scrollE !== undefined) {
+        scrollE.addEventListener("click", (event) => {
+            if (!scrollE.classList.contains("clicked")) {
+                scrollE.classList.add("clicked");
+            }
         });
     }
 
-
+    // Create dropdown for homepage feature section
+    var navThang = document.querySelectorAll(".feature-page-subnav")[0],
+        subNavThang = document.querySelectorAll(
+            ".feature-page-subnav .subnav"
+        )[0];
+    if (navThang) {
+        navThang.addEventListener("mouseenter", function (event) {
+            subNavThang.classList.toggle("active");
+        });
+        navThang.addEventListener("mouseleave", function (event) {
+            subNavThang.classList.toggle("active");
+        });
+    }
     // When clicking form input add active class to input's parents
-    document.querySelectorAll('.wpcf7-form-control-wrap *').forEach(item => {
+    document.querySelectorAll(".wpcf7-form-control-wrap *").forEach((item) => {
         // if( item ){
-            item.addEventListener('focus', event => {
-                item.parentElement.classList.toggle('active');
-            });
-            item.addEventListener('blur', event => {
-                
-                item.parentElement.classList.toggle('active');
-                if ( item.value !== '' ) {
-                    item.classList.add('valid');
-                } else{
-                    item.classList.remove('valid');
-                }
-            });
-        // }  
+        item.addEventListener("focus", (event) => {
+            item.parentElement.classList.toggle("active");
+        });
+        item.addEventListener("blur", (event) => {
+            item.parentElement.classList.toggle("active");
+            if (item.value !== "") {
+                item.classList.add("valid");
+            } else {
+                item.classList.remove("valid");
+            }
+        });
+        // }
     });
-   
     // Manipulating attorney title on archive page
-    if(document.querySelectorAll('.feature-attorney-title') != undefined){
-
-        document.querySelectorAll('.feature-attorney-title').forEach(item => {
+    if (document.querySelectorAll(".feature-attorney-title") != undefined) {
+        document.querySelectorAll(".feature-attorney-title").forEach((item) => {
             let name = item.innerHTML;
-            let pieces = name.split('. ');
-            pieces[0] = '<span class="small-title">' + pieces[0] + '.</span><br>';
-            pieces[1] = pieces[1].replace( pieces[1][0], '<span class="block-cap">' + pieces[1][0] + '</span>');
-            name = pieces.join(' ');
+            let pieces = name.split(". ");
+            pieces[0] =
+                '<span class="small-title">' + pieces[0] + ".</span><br>";
+            pieces[1] = pieces[1].replace(
+                pieces[1][0],
+                '<span class="block-cap">' + pieces[1][0] + "</span>"
+            );
+            name = pieces.join(" ");
             item.innerHTML = name;
         });
     }
-
     // Bio targetting on the attorney page
-    if(document.querySelectorAll('.bio-button') != undefined ){
-        this.document.querySelectorAll('.bio-button').forEach(item => {
-            item.addEventListener('click', event => {
-
+    if (document.querySelectorAll(".bio-button") != undefined) {
+        this.document.querySelectorAll(".bio-button").forEach((item) => {
+            item.addEventListener("click", (event) => {
                 // remove all of the classes
-                document.querySelectorAll('.active').forEach(item => { item.classList.remove('active') });
-                
+                document.querySelectorAll(".active").forEach((item) => {
+                    item.classList.remove("active");
+                });
                 // add to parent list item
-                item.parentElement.parentElement.classList.add('active');
-                
+                item.parentElement.parentElement.classList.add("active");
                 // add to corresponding description container
-                let targetID =  item.getAttribute('data-target');
+                let targetID = item.getAttribute("data-target");
                 let target = document.getElementById(targetID);
-                target.classList.add('active');
-                
+                target.classList.add("active");
             });
         });
     }
-
-
-    var boxE = document.querySelectorAll('.invisi-box');
-
-    if( boxE != undefined ){
-
-        boxE.forEach( item => {
-            item.addEventListener('mouseenter', event => {
-
+    var boxE = document.querySelectorAll(".invisi-box");
+    if (boxE != undefined) {
+        boxE.forEach((item) => {
+            item.addEventListener("mouseenter", (event) => {
                 // remove all calsses first
-                document.querySelectorAll('.hover-state').forEach( active => {active.classList.remove('hover-state')});
-
+                document.querySelectorAll(".hover-state").forEach((active) => {
+                    active.classList.remove("hover-state");
+                });
                 // update
-                item.parentElement.classList.add('hover-state');
-
-                item.classList.add('hover-state');
-                
-                
+                item.parentElement.classList.add("hover-state");
+                item.classList.add("hover-state");
             });
-
-            item.addEventListener('mouseleave', event => {
-
+            item.addEventListener("mouseleave", (event) => {
                 // remove all calsses first
-                document.querySelectorAll('.hover-state').forEach( active => { active.classList.remove('hover-state')});
-                
+                document.querySelectorAll(".hover-state").forEach((active) => {
+                    active.classList.remove("hover-state");
+                });
             });
-
         });
-
-        
     }
-    
-    
 });
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {version} = require('./package.json');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const env = process.env.NODE_ENV || 'development'
-const mode = env === 'production' ? 'production' : 'development'
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { version } = require("./package.json");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const env = process.env.NODE_ENV || "development";
+const mode = env === "production" ? "production" : "development";
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: env,
     entry: {
-        script: './src/js/script.js',
+        script: "./src/js/script.js",
         // blocks: './src/js/blocks.js',
         // style: './src/sass/style.scss',
         // styleeditor: './src/sass/style-editor.scss'
@@ -19,21 +19,23 @@ module.exports = {
     },
     resolve: {
         alias: {
-            jquery: 'jquery/src/jquery',
-        }
+            jquery: "jquery/src/jquery",
+        },
     },
     output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, './assets/js/'),
-        publicPath: path.resolve(__dirname, './'),
-        hotUpdateChunkFilename: 'hot/[hash]-hot-update.js',
-        hotUpdateMainFilename: 'hot/[hash]-hot-update.json'
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "./assets/js/"),
+        publicPath: path.resolve(__dirname, "./"),
+        hotUpdateChunkFilename: "hot/[hash]-hot-update.js",
+        hotUpdateMainFilename: "hot/[hash]-hot-update.json",
     },
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin({
-            sourceMap: true,
-        })],
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+            }),
+        ],
     },
     module: {
         rules: [
@@ -42,77 +44,78 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: [
                     {
-                        loader: 'babel-loader',
+                        loader: "babel-loader",
                         options: {
                             babelrc: true,
-                            extends: path.join(__dirname, '/.babelrc'),
+                            extends: path.join(__dirname, "/.babelrc"),
                             cacheDirectory: true,
-                        }
+                        },
                     },
                     {
-                        loader: 'webpack-import-glob-loader',
-                    }
-                    
-                ]
+                        loader: "webpack-import-glob-loader",
+                    },
+                ],
             },
             {
                 test: /\.(sa|sc|c)ss$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
-                        loader: 'css-loader',
+                        loader: "css-loader",
                         options: {
                             sourceMap: true,
                             importLoaders: 2,
-                        }
-
+                        },
                     },
                     {
-                        loader: 'postcss-loader',
+                        loader: "postcss-loader",
                         options: {
                             sourceMap: true,
-                            config: {path: './postcss.config.js'}
-                        }
+                            config: { path: "./postcss.config.js" },
+                        },
                     },
                     {
-                        loader: 'sass-loader',
+                        loader: "sass-loader",
                         options: {
-                            sourceMap: true
-                        }
+                            sourceMap: true,
+                        },
                     },
-                    'import-glob-loader',
-                ]
-            }
+                    "import-glob-loader",
+                ],
+            },
         ],
     },
-    devtool: 'source',
+    devtool: "source-map",
     plugins: [
-        new BrowserSyncPlugin({
-            files: ['**/*.css', '**/*.php'],
-            host: 'localhost',
-            port: 3000,
-            open: false,
-            proxy: 'http://capezzahill.local/'
-        },
-        {
-            reload: false
-            
-        }),
-        new webpack.HotModuleReplacementPlugin({
-
-        }),
+        new BrowserSyncPlugin(
+            {
+                files: ["**/*.css", "**/*.php"],
+                host: "localhost",
+                port: 3000,
+                open: false,
+                proxy: "http://capezzahill.local/",
+            },
+            {
+                reload: false,
+            }
+        ),
+        new webpack.HotModuleReplacementPlugin({}),
         new CleanWebpackPlugin({
             vervose: true,
-            cleanOnceBeforeBuildPatterns: ['./capezza-hill/assets/js/hot', 'script-min-*', './capezza-hill/style.css'],
+            cleanOnceBeforeBuildPatterns: [
+                "./capezza-hill/assets/js/hot",
+                "script-min-*",
+                "./capezza-hill/style.css",
+            ],
         }),
         new MiniCssExtractPlugin({
-            filename: '../../style.css',
+            filename: "../../style.css",
             // moduleFilename: ({ name }) => `${name.replace('/js/', '/css/')}.css`,
-            chunkFilename: '../../[id].css',
-          }),
+            chunkFilename: "../../[id].css",
+        }),
         new webpack.DefinePlugin({
-            APP_BASE: JSON.stringify(path.resolve(__dirname, './package.json')),
+            APP_BASE: JSON.stringify(path.resolve(__dirname, "./package.json")),
             VERSION: version,
         }),
-    ]
-}
+    ],
+};
